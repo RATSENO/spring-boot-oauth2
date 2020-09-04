286fc8f483a7f41935743401ab57d8e65f387622
RATSENO <onestar1014@gmail.com>
2020-09-04 09:37:43 +0900

    password grant

---
+++ boot-oauth2-password-authorization-server/src/main/java/com/ratseno/Application.java
@@ -0,0 +1,37 @@
+package com.ratseno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@EnableResourceServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/validateUser")
+    public Principal user(Principal user){
+        return user;
+    }
+
+    @Configuration
+    protected static class AuthenticationManagerConfiguration extends GlobalAuthenticationConfigurerAdapter{
+        @Override
+        public void init(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication()
+                    .withUser("ratseno-user")
+                    .password("ratseno-pass")
+                    .roles("USER");
+        }
+    }
+}
